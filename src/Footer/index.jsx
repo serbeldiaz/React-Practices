98c07229829573179fb1e227709e359c0a563fc2
serbeldiaz <serbeldiaz@infracommerce.lat>
2023-02-20 01:19:13 -0500

    [202302200119] Feat: Create Footer

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -1,9 +1,22 @@
 import './index.css';
+import Isotipo from './Isotipo.svg'
 
 function Footer() {
   return (
-    <div className="App">
-      
+    <div className="footer">
+      <div className='footer-container-inner'>
+        <div className='isotipo'>
+          <img src={Isotipo} alt="Isotipo" />
+        </div>
+
+        <div className='copyright'>
+          @ 2023 Sergio Beleño Inc. All rights reserved
+        </div>
+
+        <div className='isotipo'>
+          <img src={Isotipo} alt="Isotipo" />
+        </div>
+      </div>
     </div>
   );
 }
